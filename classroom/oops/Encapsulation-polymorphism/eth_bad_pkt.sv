a0e513b0ebedc3d69b2790fbdd8e19705047f4b6
data0xz <ee17m085@alumni.iitm.ac.in>
2024-11-05 20:22:56 +0530

    bad pkt

--- classroom/oops/Encapsulation-polymorphism/eth_bad_pkt.sv
+++ classroom/oops/Encapsulation-polymorphism/eth_bad_pkt.sv
@@ -1,15 +1,16 @@
 class eth_bad_pkt extends eth_pkt;
 
- static count_bad;
+  static int count_bad;
 
- function new();
-  count_bad++;
- endfunction 
+  function new();
+    count_bad++;
+  endfunction 
 
- function void print(string name="eth_pkt");
-  super.print(name);
-	$display("\t count_bad=%0d",count_bad);
- endfunction
+  function void print(string name="eth_pkt");
+    super.print(name);
+    $display("\t count_bad=%0d",count_bad);
+  endfunction
 
 
 endclass
+
