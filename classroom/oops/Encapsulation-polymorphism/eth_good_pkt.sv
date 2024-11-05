1717fe4602ff6e7f7e0e9f83f74a335ef690e9e4
data0xz <ee17m085@alumni.iitm.ac.in>
2024-11-05 20:23:43 +0530

    good pkt

--- classroom/oops/Encapsulation-polymorphism/eth_good_pkt.sv
+++ classroom/oops/Encapsulation-polymorphism/eth_good_pkt.sv
@@ -1,15 +1,16 @@
 class eth_good_pkt extends eth_pkt;
 
- static count_good;
+  static int count_good;
 
- function new();
-  count_good++;
- endfunction 
+  function new();
+    count_good++;
+  endfunction 
 
- function void print(string name="eth_pkt");
-  super.print(name);
-	$display("\t count_good=%0d",count_good);
- endfunction
+  function void print(string name="eth_pkt");
+    super.print(name);
+    $display("\t count_good=%0d",count_good);
+  endfunction
 
 
 endclass
+
