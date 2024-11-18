10d0ac6f71a13e2d55ab00223404546f24fadcf1
data0xz <ee17m085@alumni.iitm.ac.in>
2024-11-18 12:19:51 +0530

    child class pkt print & total pkt print in top module

---
+++ classroom/oops/Encapsulation-polymorphism/var_1/eth_bfm.sv
@@ -0,0 +1,16 @@
+class eth_bfm;
+
+
+  eth_pkt pkt;
+
+  task run();
+    repeat(10) begin
+      eth_common::gen2bfm_mb.get(pkt);
+      pkt.print();
+    end
+  endtask
+
+
+
+endclass
+
---
+++ classroom/oops/Encapsulation-polymorphism/var_1/eth_ill_pkt.sv
@@ -0,0 +1,16 @@
+class eth_ill_pkt extends eth_pkt;
+
+  static int count_ill;
+
+  function new();
+    count_ill++;
+  endfunction 
+
+   function void print(string name);
+     super.print("eth_ill_pkt");
+    $display("\t count_ill=%0d",count_ill);
+  endfunction
+
+
+endclass
+
