d826a49eabf4ca1e0eacd483f85d1467a3d7dbb7
data0xz <ee17m085@alumni.iitm.ac.in>
2024-11-26 03:39:26 +0530

    this , super inheritance example

---
+++ classroom/oops/this_super_inheritance/eth_good_pkt.sv
@@ -0,0 +1,41 @@
+`include "eth_pkt.sv"
+class eth_good_pkt extends eth_pkt;
+
+  static int count_good;
+
+  integer name=8;
+
+  function new();
+    count_good++;
+  endfunction
+
+  function void print_good();
+    
+    super.print("parent through child class");
+    $display("\tcount_good=%0d",count_good);
+    $display("\tcount=%0d",count);
+    $display("\t%s",super.name);
+    $display("\t%0d",this.name);
+    $display("\t%0d",name);
+    
+  endfunction
+
+endclass
+
+module tb;
+
+  eth_good_pkt g_pkt;
+
+  initial
+    begin
+      repeat(5)
+        begin
+          g_pkt=new();
+          assert(g_pkt.randomize());
+          g_pkt.print_good();
+          g_pkt.print();
+        end
+
+    end
+
+endmodule
---
+++ classroom/oops/this_super_inheritance/eth_pkt.sv
@@ -0,0 +1,28 @@
+class eth_pkt;
+
+  //properties
+
+  string name="parent";
+  static int count;
+  rand bit[9:0] len;
+  rand byte payload[$];
+
+  //methods
+  function new();
+    count++;
+  endfunction
+
+  function void print(string name="parent class");
+    $display("\n\tprinting %s",name);
+    $display("\tcount=%0d",count);
+    $display("\tlen=%0d",len);
+    $display("\tpayload=%p",payload);
+  endfunction
+
+  //constraints
+  constraint payload_c{
+    payload.size()==len;
+    len inside {[5:10]};
+  }
+
+endclass
