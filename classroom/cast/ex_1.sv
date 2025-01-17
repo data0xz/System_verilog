fecc09462eb7102673ad9f5f709c18bd10ea0811
data0xz <ee17m085@alumni.iitm.ac.in>
2025-01-17 11:21:57 +0530

    static casting ex

---
+++ classroom/cast/ex_1.sv
@@ -0,0 +1,29 @@
+typedef enum bit[1:0]{  
+  GOOD,
+  BAD,
+  ILL 
+}pkt_type_t;
+
+
+module tb;
+
+  pkt_type_t pkt_type;
+  int a;
+
+  initial
+    begin
+      pkt_type=ILL;
+      a=int'(pkt_type);
+
+      $display("a=%0d",a);
+      
+      a=1;
+      pkt_type=pkt_type_t'(a);      
+      $display("pkt_type=%s",pkt_type);
+      
+      
+    end
+
+endmodule
+
+
